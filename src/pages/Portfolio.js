12cce5a2dbd94c347bf3ccba9634bf717609f2fc
Gantulga0722 <mgtulga22@gmail.com>
2024-01-17 00:06:46 +0800

    From Home

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,24 @@
+import {
+  Header,
+  Hero,
+  About,
+  Skills,
+  Experience,
+  Work,
+  ContactMe,
+  Footer,
+} from "@/components/Index";
+const Portfolio = () => {
+  return (
+    (<Header />),
+    (<Hero />),
+    (<About />),
+    (<Skills />),
+    (<Experience />),
+    (<Work />),
+    (<ContactMe />),
+    (<Footer />)
+  );
+};
+
+export default Portfolio;
